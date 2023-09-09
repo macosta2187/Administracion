326517946cc2ad199f3c174675179566aa088ddd
macosta2187 <76165163+macosta2187@users.noreply.github.com>
2023-09-08 23:25:37 -0300

    Insertar Vehiculo

---
+++ app/Http/Controllers/VehiculoController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Vehiculo;
+
+
+class VehiculoController extends Controller
+{
+    public function Insertar(Request $request)
+    {
+
+        $vehiculo = new Vehiculo;
+        $vehiculo->matricula = $request->input('matricula');
+        $vehiculo->marca = $request->input('marca');
+        $vehiculo->modelo = $request->input('modelo');
+        $vehiculo->tipo = $request->input('tipo');
+        $vehiculo->capacidad_peso = $request->input('capacidad_peso');        
+        $vehiculo->id_chofer = $request->input('id_chofer');
+        $vehiculo->save();
+    }
+}
