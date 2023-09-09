b71a20f2e57d5e3da9aa1fc9cdba884a75342859
macosta2187 <76165163+macosta2187@users.noreply.github.com>
2023-09-08 23:28:12 -0300

    Actualizar vehiculo

--- app/Http/Controllers/VehiculoController.php
+++ app/Http/Controllers/VehiculoController.php
@@ -33,4 +33,18 @@ class VehiculoController extends Controller
     {
         return view('vehiculos.Editar', compact('vehiculo'));
     }
+
+    public function Actualizar(Request $request, Vehiculo $vehiculo)
+    {
+        $vehiculo = new Vehiculo;
+        $vehiculo->matricula = $request->input('matricula');
+        $vehiculo->marca = $request->input('marca');
+        $vehiculo->modelo = $request->input('modelo');
+        $vehiculo->tipo = $request->input('tipo');
+        $vehiculo->capacidad_peso = $request->input('capacidad_peso');        
+        $vehiculo->id_chofer = $request->input('id_chofer');
+        $vehiculo->save();
+
+        return redirect("/");
+    }
 }
